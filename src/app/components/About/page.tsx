0e4e9ef08f4fb90e36ac2477c2b12d15c2218292
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-07-21 22:41:53 +0300

    write about component and import resume tamplate

---
+++ src/app/components/About/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { FaCloudDownloadAlt } from 'react-icons/fa';
+import { TextLoop } from 'react-text-loop-next';
+
+const About = () => {
+	return (
+		<div className="about">
+			<div className="about_info">
+				<h1>
+					<TextLoop>
+						<span style={{ color: 'var(--pink-color)' }}>Front-end Developer</span>
+						<span style={{ color: '#ff8906' }}>Graphic Designer</span>
+						<span style={{ color: '#4285F4' }}>Web Developer</span>
+					</TextLoop>
+				</h1>
+				<p>{`Hi, I'm Ich1ro. A passionate Web Developer based in Ukraine. 📍`}</p>
+				<div className="resume">
+					<a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+						Resume <FaCloudDownloadAlt className="tooltip-svg" />
+					</a>
+				</div>
+			</div>
+			<div className="about_img"></div>
+		</div>
+	);
+};
+
+export default About;
