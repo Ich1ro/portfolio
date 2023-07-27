e0f2ce078f206e21d8dd9ee5ede9ce083ada1527
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-07-27 15:47:51 +0300

    social media data

---
+++ src/app/data/SocialMediaLinks.ts
@@ -0,0 +1,26 @@
+import {
+	SiTelegram,
+	SiGithub,
+	SiLinkedin,
+	SiGmail
+} from 'react-icons/si';
+import { SocialMediaLink } from '../interfaces';
+
+export const SocialMediaLinks: SocialMediaLink[] = [
+	{
+		icon: SiGmail,
+		url: 'mailto:pavel.dubenchuk.02@gmail.com'
+	},
+	{
+		icon: SiGithub,
+		url: 'https://github.com/Ich1ro'
+	},
+	{
+		icon: SiTelegram,
+		url: 'https://t.me/qqcold'
+	},
+	{
+		icon: SiLinkedin,
+		url: 'https://www.linkedin.com/in/pavel-dubenchuk/'
+	},
+];
