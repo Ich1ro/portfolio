e90a04beeddf53afc0433674c70c2782c9a9bdf9
ich1ro <pavel.dubenchuk.02@gmail.com>
2023-07-29 15:16:19 +0300

    change image source in projects array

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -5,7 +5,7 @@ export const projects = [
         title: 'Weather Application',
         content: `Weather App is a web application that provides users with the ability to view weather forecasts for their region.`,
         link: 'https://ich1ro.github.io/react-weather-app/',
-        image: 'WeatherApp.png',
+        image: 'WeatherApp.webp',
         stack: ['TypeScript', 'React', 'Redux', 'NextJS', 'Axios', 'Github', 'Babel', 'Jest'],
         github: 'https://github.com/Ich1ro/react-weather-app'
     } as projectType,
@@ -13,7 +13,7 @@ export const projects = [
         title: 'Full stack E-commerce',
         content: `This project is a full-stack e-commerce website built with React, Next.js, Sanity, and Stripe. The website allows users to browse and purchase products, and it also features a checkout system that allows users to pay for their purchases with credit cards.`,
         link: 'https://fullstack-ecommerce-project-z4cj.vercel.app/',
-        image: 'ECommerce.png',
+        image: 'ECommerce.webp',
         stack: ['JavaScript', 'React', 'NextJS', 'Github', 'Babel', 'Stripe', 'Vercel'],
         github: 'https://github.com/Ich1ro/fullstack-ecommerce-project'
     } as projectType,
@@ -21,7 +21,7 @@ export const projects = [
         title: 'Pinterest clone',
         content: `This project is a Pinterest clone built with React, Sanity, Google Auth, and Stripe. It allows users to create boards, pin images, and save pins from other users. The project is fully functional and can be used to create a Pinterest-like social media platform.`,
         link: 'https://pinterest-clone.netlify.app/',
-        image: 'PinterestClone.png',
+        image: 'PinterestClone.webp',
         stack: ['JavaScript', 'React', 'TailwindCSS', 'Github', 'Netlify', 'GCP'],
         github: 'https://github.com/Ich1ro/pinterest-clone'
     } as projectType,
@@ -29,7 +29,7 @@ export const projects = [
         title: 'React form',
         content: `This project is a doctor appointment form built with TypeScript, React, Next.js, and React-hook-form. It allows users to book appointments with doctors, and it also features a search bar that allows users to find doctors by name, specialty, and location.`,
         link: 'https://react-form-ich1ro.vercel.app/',
-        image: 'ReactForm.png',
+        image: 'ReactForm.webp',
         stack: ['TypeScript', 'React', 'NextJS', 'Axios', 'Github', 'Vercel'],
         github: 'https://github.com/Ich1ro/react-form'
     } as projectType,
@@ -37,7 +37,7 @@ export const projects = [
         title: 'Salesforce Todo List',
         content: `This project is a to-do list app built with Salesforce, Apex, JavaScript, and Lightning Web Components. It allows users to create tasks, set deadlines, and track their progress.`,
         link: 'https://wise-unicorn-c1dtu5-dev-ed.my.site.com/',
-        image: 'TodoList.png',
+        image: 'TodoList.webp',
         stack: ['JavaScript', 'Salesforce', 'Github'],
         github: 'https://github.com/Ich1ro/salesforce-todo-list'
     } as projectType,
@@ -45,7 +45,7 @@ export const projects = [
         title: 'Portfolio',
         content: `This project is a personal portfolio website built with TypeScript, React, Next.js, and SCSS. It showcases my work and experience.`,
         link: '',
-        image: 'Portfolio.png',
+        image: 'Portfolio.webp',
         stack: ['TypeScript', 'React', 'NextJS', 'Saas', 'Github', 'Vercel'],
         github: 'https://github.com/Ich1ro/portfolio'
     } as projectType,
